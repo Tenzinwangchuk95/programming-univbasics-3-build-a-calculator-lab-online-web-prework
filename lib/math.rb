551a0a269e1fb23f05cd4cbc123abd51ae41c010
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-03 20:16:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,25 +6,21 @@ end
 def subtraction(num1, num2)
   subtraction = num1 - num2
   p subtraction
-
 end
 
 def division(num1, num2)
   division = num1 / num2
   p division
-
 end
 
 def multiplication(num1, num2)
   multiplication = num1 * num2
   p multiplication
-
 end
 
 def modulo(num1, num2)
   modulo = num2 % num1
   p modulo
-
 end
 
 def square_root(num)
