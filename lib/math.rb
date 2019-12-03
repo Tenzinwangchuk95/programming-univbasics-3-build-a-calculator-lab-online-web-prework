5e2e13778d021674b2432e50dcd71100806d61e9
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-03 20:14:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,14 +4,20 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  subtraction = num1 - num2
+  p subtraction
 
 end
 
 def division(num1, num2)
+  division = num1 / num2
+  p division
 
 end
 
 def multiplication(num1, num2)
+  multiplication = num1 * num2
+  p multiplication
 
 end
 
